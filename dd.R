a40cc40564c43a3c1b7700a23f313dd23af3275b
Hun Kim <kimhon@yonsei.ac.kr>
2022-09-09 14:26:41 +0000

    first commmmm

---
+++ dd.R
@@ -0,0 +1,28 @@
+install.packages("usethis")
+
+### 3. Configure git with Rstudio ############################################
+
+## Name, email
+usethis::use_git_config(user.name = "Hun Kim", user.email = "kimhon@yonsei.ac.kr")
+
+## setting: permanent storage
+credentials::credential_helper_set("manager")                 # Windows
+credentials::credential_helper_set("osxkeychain")             # Mac
+credentials::credential_helper_set("store")                   # Linux
+
+## create a personal access token for authentication:
+usethis::create_github_token() 
+## in case usethis version < 2.0.0: usethis::browse_github_token() (or even better: update usethis!)
+
+## copy and paste your token and press enter
+credentials::set_github_pat()
+
+# ----------------------------------------------------------------------------
+
+#### 4. Restart R! ###########################################################
+
+# ----------------------------------------------------------------------------
+
+#### 5. Verify settings ######################################################
+
+usethis::git_sitrep()
