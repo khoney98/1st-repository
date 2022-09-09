63a75640b1e27d911e0905d97fd9682f8c299763
Hun Kim <kimhon@yonsei.ac.kr>
2022-09-09 15:39:38 +0000

    cloud

--- dd.R
+++ dd.R
@@ -15,7 +15,7 @@ usethis::create_github_token()
 ## in case usethis version < 2.0.0: usethis::browse_github_token() (or even better: update usethis!)
 
 ## copy and paste your token and press enter
-credentials::set_github_pat()
+credentials::set_github_pat("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")
 
 # ----------------------------------------------------------------------------
 
